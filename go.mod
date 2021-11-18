d8d75836fcce3462469b55bd7dea3cc76a818e36
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-11-17 21:11:35 -0800

    refactor

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/andrewmarklloyd/pi-app-updater
 
 go 1.16
 
-require github.com/spf13/cobra v1.2.1 // indirect
+require (
+	github.com/robfig/cron/v3 v3.0.1
+	github.com/spf13/cobra v1.2.1
+)
--- main.go
+++ main.go
@@ -1,22 +1,81 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package main
 
-import "github.com/andrewmarklloyd/pi-app-updater/update/cmd"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/robfig/cron/v3"
+)
+
+type AppInfo struct {
+	TagName string `json:"tag_name"`
+}
+
+const (
+	interval = 2
+)
 
 func main() {
-	cmd.Execute()
+	args := os.Args[1:]
+	releaseURL := args[0]
+	updateScript := args[1]
+
+	var cronLib *cron.Cron
+	cronLib = cron.New()
+	cronLib.AddFunc(fmt.Sprintf("@every %ds", interval), func() {
+		err := checkForUpdates(releaseURL, updateScript)
+		if err != nil {
+			fmt.Println("Error checking for updates:", err)
+		}
+	})
+	cronLib.Start()
+
+	go forever()
+	select {} // block forever
+}
+
+func checkForUpdates(releaseURL string, updateScript string) error {
+	log.Println("Checking for updates")
+	resp, err := http.Get(releaseURL)
+	if err != nil {
+		return err
+	}
+	var info AppInfo
+	err = json.NewDecoder(resp.Body).Decode(&info)
+	if err != nil {
+		return fmt.Errorf("parsing version from api response: %s", err)
+	} else {
+		latestVersion := []byte(info.TagName)
+		err = ioutil.WriteFile("./.latestVersion", latestVersion, 0644)
+		if err != nil {
+			return fmt.Errorf("writing latest version to file: %s", err)
+		}
+
+		version, err := ioutil.ReadFile("./.version")
+		if err != nil {
+			return fmt.Errorf("reading current version from file: %s", err)
+		}
+
+		if info.TagName != string(version) {
+			fmt.Println("New version available, updating now")
+			out, err := exec.Command(updateScript).Output()
+			if err != nil {
+				return fmt.Errorf("initiating command: %s", err)
+			}
+			fmt.Println(string(out))
+		}
+	}
+	return nil
+}
+
+func forever() {
+	for {
+		time.Sleep(5 * time.Minute)
+	}
 }
