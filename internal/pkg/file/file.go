7338108547348079499a15d787997ad0154cef9a
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-06-10 20:15:35 -0700

    Add more context to errors when copying file

--- internal/pkg/file/file.go
+++ internal/pkg/file/file.go
@@ -13,29 +13,29 @@ const (
 func doCopyWithOwnership(src, dest string) error {
 	source, err := os.Open(src)
 	if err != nil {
-		return err
+		return fmt.Errorf("opening source file: %s", err)
 	}
 	defer source.Close()
 
 	destination, err := os.Create(dest)
 	if err != nil {
-		return err
+		return fmt.Errorf("opening destination file: %s", err)
 	}
 	defer destination.Close()
 	_, err = io.Copy(destination, source)
 	if err != nil {
-		return err
+		return fmt.Errorf("copying source to destination file: %s", err)
 	}
 
 	err = os.Chown(dest, defaultUserID, defaultUserID)
 	if err != nil {
-		return err
+		return fmt.Errorf("changing ownership of file: %s", err)
 	}
 	return nil
 }
 
-func CopyWithOwnership(srdDestMap map[string]string) error {
-	for s, d := range srdDestMap {
+func CopyWithOwnership(srcDestMap map[string]string) error {
+	for s, d := range srcDestMap {
 		err := doCopyWithOwnership(s, d)
 		if err != nil {
 			return err
